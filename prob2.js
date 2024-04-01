5b26ad07f152d67a9bcd33bf69dfee6a999532a7
snubin <jeonsubin5156@inha.edu>
2024-04-01 20:50:50 +0900

    complete

--- prob2.js
+++ prob2.js
@@ -1,1 +1,15 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(n){
+    let result=1;
+    
+    for(let i=1;i<11;i++){
+      result=result*i
+      
+  if(n<result){
+      return i-1;
+  }
+  else if(n===result) {
+       return i;
+  }
+    }
+}
--- prob3.js
+++ prob3.js
@@ -1,1 +1,19 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+    let n=arr.length
+    
+    if (n<=1){
+        return [-1];
+    }
+    let comp = 2147483647;
+    let idex= 0;
+      for(let i=0;i<n;i++){
+          if(arr[i]<comp){
+              comp=arr[i];
+              idex=i;
+          }
+      }
+    
+    arr.splice(idex,1);
+    return arr;
+}
